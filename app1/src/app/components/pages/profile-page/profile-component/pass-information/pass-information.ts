849244e13280a1cab2d11e982461bbfc5d685d59
Nosachenko Mykola <nosachenko.mykola96@gmail.com>
2026-03-10 18:10:54 +0200

    fixed styles

--- app1/src/app/components/pages/profile-page/profile-component/pass-information/pass-information.ts
+++ app1/src/app/components/pages/profile-page/profile-component/pass-information/pass-information.ts
@@ -1,17 +1,24 @@
 import { Component } from '@angular/core';
-import { IconFieldModule } from 'primeng/iconfield';
-import { InputIconModule } from 'primeng/inputicon';
+import { PasswordModule } from 'primeng/password';
+import { FormsModule } from '@angular/forms';
+import { FloatLabelModule } from 'primeng/floatlabel';
 
 @Component({
   selector: 'app-pass-information',
-  imports: [IconFieldModule, InputIconModule],
+  imports: [ PasswordModule, FormsModule, FloatLabelModule ],
   templateUrl: './pass-information.html',
-  styleUrl: './pass-information.scss',
+  styleUrls: ['./pass-information.scss'],
 })
 export class PassInformation {
-  labels: string[] = [
-    'Current Password:', 
-    'New Password:', 
-    'Confirm New Password:'
+  passwords: Record<string, string> = {
+    current: '',
+    new: '',
+    confirm: ''
+  };
+
+  fields = [
+    { id: 'current', label: 'Current Password:' },
+    { id: 'new', label: 'New Password:' },
+    { id: 'confirm', label: 'Confirm New Password:' }
   ];
 }
